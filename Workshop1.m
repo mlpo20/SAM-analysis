231b985751ee1986bb413b041a9ffaeaa82f516e
mlpo20 <71485869+mlpo20@users.noreply.github.com>
2020-09-18 11:46:22 +0200

    Update Workshop1.m

--- Workshop1.m
+++ Workshop1.m
@@ -1,7 +1,16 @@
 %Workshop 1
 
 
+clc
 
-fname = "data/X031Y094.csv"
-data = csvread(fname)
-plot(data)
+for x = 1:125
+    for y = 1:125
+        Y = num2str(y,'%03d');
+        X = num2str(x,'%03d');
+        data = csvread(['data/','X',X,'Y',Y,'.csv']);
+        
+        z(x,y) = max(data);
+    end
+end
+
+surf(z)
